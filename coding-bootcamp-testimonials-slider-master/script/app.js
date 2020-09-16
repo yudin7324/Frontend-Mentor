96b85aa1dc7bf4c78b9a0918a1bfa62d6087fa92
Ilya <yudin2401@gmail.com>
2020-09-16 17:05:10 +0300

    done

---
+++ coding-bootcamp-testimonials-slider-master/script/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        easing: 'linear',
+        fade: true,
+        adaptiveHeight: true,
+        zIndex: 500,
+        // nextArrow: document.querySelector('.slider__button-prev'),
+        // prevArrow: document.querySelector('.slider__button-next'),
+    });
+
+
+
+});
